234c46797fd039d2e50130bd9a270aca13a9a365
Felipe <machadofelipe2016@outlook.com>
2022-08-06 13:34:21 -0300

    c12-create  another router to put date and verify my users

--- backend/Routes/Userroutes.js
+++ backend/Routes/Userroutes.js
@@ -2,21 +2,25 @@ const express = require("express")
 const router = express.Router()
 
 //controller 
-const {register, login, getCurrentUser} = require("../controllers/Usercontrolle") 
+const {register, login, getCurrentUser, update} = require("../controllers/Usercontrolle") 
 
 //middleware
 const validate = require('../middleware/handleValidation')
 const {userCreateValidation,
-     loginValidation} = require("../middleware/User.validations")
+     loginValidation,
+     userUpdateValidation
+} = require("../middleware/User.validations")
 
 const AuthGuard = require("../middleware/authGuard")
+const { uploadImage } = require("../middleware/imageUpload")
 
 
 //router
 router.post("/register", userCreateValidation(), validate,register)
 router.post("/login", loginValidation(), validate,login)
 router.get("/profile", AuthGuard,getCurrentUser)
-
+router.put("/", AuthGuard, userUpdateValidation(), validate , uploadImage.single("profileImage"),
+update)
 
 
 // this use i need agreed here 
