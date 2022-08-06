7eacd1db2a09116ad8df7dcf8446bc141261a821
Felipe <machadofelipe2016@outlook.com>
2022-08-06 11:38:59 -0300

    c6-rearrange code do something test in code

--- backend/Routes/Userroutes.js
+++ backend/Routes/Userroutes.js
@@ -2,16 +2,27 @@ const express = require("express")
 const router = express.Router()
 
 //controller 
-const {register} = require("../controllers/Usercontrolle") 
+const {register, login, getCurrentUser} = require("../controllers/Usercontrolle") 
 
 //middleware
 const validate = require('../middleware/handleValidation')
-const {userCreateValidation} = require("../middleware/User.validations")
+const {userCreateValidation,
+     loginValidation} = require("../middleware/User.validations")
+
+const AuthGuard = require("../middleware/authGuard")
+
 
 //router
-router.post("/register", userCreateValidation(), validate ,register)
+router.post("/register", userCreateValidation(), validate,register)
+router.post("/login", loginValidation(), validate,login)
+router.get("/profile", AuthGuard,getCurrentUser)
+
+
 
 // this use i need agreed here 
-router.use("/api/users/register", register )
+router.use("/api/users/register", register)
+router.use("api/users/login",login)
+//router.use("api/users/profile", getCurrentUser)
+
 
 module.exports = router;
