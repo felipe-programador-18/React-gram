93772a1c3e7689db98e02d1188f443f15a94f3eb
Felipe <machadofelipe2016@outlook.com>
2022-08-06 11:35:29 -0300

    c3-created folder call authguard to verify  if token exist or not exist

---
+++ backend/middleware/authGuard.js
@@ -0,0 +1,29 @@
+const User = require("../models/User")
+const jwt = require("jsonwebtoken")
+const jwtSecret = process.env.JWT_SECRET;
+
+
+const AuthGuard = async (req, res, next) =>{
+
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(" ")[1];
+
+    // check if was a token
+    if(!token) return res.status(401).json({errors:["Acesso negado! "]});
+
+    // now i need check my token is validate!!
+     
+    try{
+      const verified = await jwt.verify(token, jwtSecret);
+      req.user = await User.findById(verified.id).select("-password")
+      console.log("teste authGuard", req.user)
+      next();
+    }catch(error){
+       res.status(400).json({errors:['Token inválido.']})
+  
+    }
+
+}
+
+
+module.exports = AuthGuard;
