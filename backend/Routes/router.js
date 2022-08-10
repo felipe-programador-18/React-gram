3e35e4c1faf64c955714799ff6c6964233bd09d3
Felipe <machadofelipe2016@outlook.com>
2022-08-10 15:11:00 -0300

    c28-using my routeruse about photo

--- backend/Routes/router.js
+++ backend/Routes/router.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 //seems about this controllers
 router.use("/api/users", require("./Userroutes") )
+router.use("/api/photos", require("./PhotoRoutes") )
+
 
 router.get("/", (req,res) => {
     res.send("Api Working Here now!!")
