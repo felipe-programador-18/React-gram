8be25286a428747de56e09d56d50b459a39e8119
Felipe <machadofelipe2016@outlook.com>
2022-08-10 15:09:49 -0300

    c27-creating all structure about photocontrollers

--- backend/controllers/Photocontrollers.js
+++ backend/controllers/Photocontrollers.js
@@ -1,1 +1,20 @@
-const User = require("../models/photo")
+const PhotoUser = require("../models/photo")
+const mongoose = require("mongoose")
+
+
+// Insert picture User here!!
+
+const InsertPhoto = async (req,res) => {
+  const{title} = req.body;
+ console.log("test", title)
+  const image =  req.file.filename
+  console.log("test fdsfs", image)
+  console.log(req.body)
+   
+  res.send("Photo inserted!!")
+}
+
+
+module.exports = {
+    InsertPhoto
+}
