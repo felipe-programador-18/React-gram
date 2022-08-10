c64bfacb2607f1b2c53ea3c263883464e2246d9b
Felipe <machadofelipe2016@outlook.com>
2022-08-10 15:12:19 -0300

    c30-adding another controllers and creating some test here

--- backend/Routes/PhotoRoutes.js
+++ backend/Routes/PhotoRoutes.js
@@ -1,19 +1,33 @@
 const express = require("express")
-const route = express.Router();
+const router = express.Router();
 
 //controlled
+const { InsertPhoto } = require("../controllers/Photocontrollers")
 
+//testing here
+const {register} = require("../controllers/Usercontrolle")
 
 
 //middleware
 const  {photoInsertValidation} = require('../middleware/photoValidation')
-
+const {uploadImage} = require("../middleware/imageUpload")
 const authGuard = require("../middleware/authGuard")
 const validate = require("../middleware/handleValidation")
 
+
 //route
+router.post("/" ,authGuard,
+ uploadImage.single("image")
+ , photoInsertValidation()
+ , validate
+ ,InsertPhoto)
 
 
 
 
-module.exports =  route ;
+
+ //testing somethings
+ //router.use("/api/photos",register, InsertPhoto)
+
+
+module.exports =  router ;
