e2751a7ec86142ca92688229a6c2b08f2ac80a4a
Felipe <machadofelipe2016@outlook.com>
2022-08-14 13:13:52 -0300

    c42-adding more three function deletedpicutre, getallphoto, getUserphoto

--- backend/controllers/Photocontrollers.js
+++ backend/controllers/Photocontrollers.js
@@ -32,7 +32,72 @@ const InsertPhoto = async (req,res) => {
 
 }
 
+//created new users to deleted picture
+
+const DeletedPicture =  async( req, res) => {
+    const {id} = req.params ;
+    console.log("testing my parameter!", id)
+    const reqUser = req.user;
+    
+    console.log("testing about my code", reqUser )
+
+   try{ 
+   
+    const photo = await PhotoUser.findById(mongoose.Types.ObjectId(id))
+     
+    // verify if photo exist 
+     if(!photo){
+        res.status(404).json({errors: ["Foto não encontrada"]})
+        return ;
+      }
+       
+    // check if picture belongs to user
+    
+    if(!photo.userId.equals(reqUser._id)){
+        res.status(422).json({errors:['Ocorreu um erro, por favor tente novamente mais tarde.']})
+    }
+
+      await PhotoUser.findByIdAndDelete(photo._id)
+
+    res
+    .status(200)
+    .json({id:photo._id, message:' Foto excluída com sucesso.'})
+    
+
+    }catch(err){
+    
+    res.status(404).json({errors:['Ocorreu um erro,tente daqui a pouco.']})
+    
+    return; 
+    } 
+}
+
+// get all photos!!
+const GetAllPhoto = async (req,res) =>{
+  
+  const photos = await PhotoUser.find({})
+  .sort([["createdAt", -1]])
+  .exec();
+
+  return res.status(200).json(photos)
+}
+
+//get user photos
+
+const GetUserPhoto = async(req, res) => {
+  const {id}= req.params ;
+
+  const photo = await PhotoUser
+  .find({userId: id})
+  .sort([["createdAt", -1]]).exec()
+
+  return res.status(200).json(photo)
+
+}
 
 module.exports = {
-    InsertPhoto
+    InsertPhoto,
+    DeletedPicture,
+    GetAllPhoto,
+    GetUserPhoto
 }
