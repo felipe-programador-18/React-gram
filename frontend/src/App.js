ad7ddfc6ac5e1dca3e799b0dde14172d6b6c6326
Felipe <machadofelipe2016@outlook.com>
2022-08-16 16:52:55 -0300

    c60-installed all redux and another things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,42 @@
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+
+
+//pages
+import Home from './Pages/Home/Home';
+import LoginUser from './Pages/Auth/login';
+import RegisterAnother from './Pages/Auth/AnotherRegister';
+
+
+
+//components!!
+import Navbar from './Component/navbar';
+import Footer from './Component/footer';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Gran</h1>
+     <div className="App">
+     <BrowserRouter>
+
+       <Navbar/>
+       
+        <div className='container' > 
+         <Routes>       
+           <Route path='/' element={<Home/>} />
+        
+           <Route path='/login' element={ <LoginUser/> } />
+           <Route path='/register' element={<RegisterAnother/>}   />
+     
+         </Routes> 
+       </div>
+       
+       <Footer/>
+      </BrowserRouter> 
     </div>
+
   );
 }
 
