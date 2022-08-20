e2c3ff1223eb693c924258deb1df920212f3e986
Felipe <machadofelipe2016@outlook.com>
2022-08-20 15:02:22 -0300

    c69-adding provider to manage all aplication and state too

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 
 
+//create all structure about redux
+
+import { Provider } from 'react-redux'
+
+import {store} from './story'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider story ={store} >
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
