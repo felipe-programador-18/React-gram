0eec2cab71cb0b7d04cc561cec5b2415ba29cef0
Felipe <machadofelipe2016@outlook.com>
2022-08-20 14:20:28 -0300

    c64-starting created form state

--- frontend/src/Pages/Auth/AnotherRegister.js
+++ frontend/src/Pages/Auth/AnotherRegister.js
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import "./auth.css"
+import { Link, Navigate } from 'react-router-dom'
+
 
 const RegisterAnother = () => {
-    
-    return(<>
-    <h1>Register</h1>
-    </>)
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [corfirmPass , setConfirmPass] = useState("")
+   
+  const [formError, setError] = useState(false)
+  
+  //const navigate = Navigate()
+
+  
+  const HandSubmit =(e) => {
+    e.preventDefault()
+
+
+
+
+  // navigate("/")
+  }
+   
+  useEffect(() => {
+
+  },[])
+
+
+
+  return(<div id='register' >
+         <h2>ReactGram.</h2>
+        <p>Cadastre-se para ver fotos dos seus amigos.</p>
+        <form onSubmit={HandSubmit} >
+          <label> Seu Nome:</label>
+          <input name='nome'
+            type='text'
+            placeholder='Nome'
+            value={name} 
+            onChange={(e) => setName(e.target.value)} 
+            required
+            />
+          
+          <label> Seu email:</label>
+          <input name='e-mail'
+            type='text'
+            placeholder='E-mail'
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+            required
+            />
+
+         <label> Defina uma Senha:</label>
+          <input name='password'
+            type='text'
+            placeholder='Password'
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            required
+            />
+
+          <label> Confirma sua Senha:</label>
+          <input name='confirmpassword'
+            type='text'
+            placeholder='Confirme sua senha.'
+            value={corfirmPass} 
+            onChange={(e) => setName(e.target.value)} 
+            required
+            />
+          
+          <input type="submit" value='cadastrar' />
+        </form>
+        
+        <p>Já possui conta? <Link to='/login'>Click aqui.</Link></p>
+
+    </div>)
 }
 
 export default RegisterAnother
