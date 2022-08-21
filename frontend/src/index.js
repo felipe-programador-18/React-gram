10446946c29d336dfec7af422d44f9796fa212d4
Felipe <machadofelipe2016@outlook.com>
2022-08-21 13:27:24 -0300

    c77-change name inside provider to store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,7 +13,7 @@ import {store} from './story'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider story ={store} >
+    <Provider store ={store} >
     <App />
     </Provider>
   </React.StrictMode>
