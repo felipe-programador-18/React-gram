b9c7718b43fcac6505a99cae83e1609b4ba1612b
Felipe <machadofelipe2016@outlook.com>
2022-08-21 13:25:37 -0300

    c75-was created all structure to received my request

---
+++ frontend/src/services/authServices.js
@@ -0,0 +1,29 @@
+import { api ,  requestConf } from '../utils/config'
+
+//register user in system!!!
+const registeUser =  async (data) =>{
+ const config = requestConf("POST", data)
+
+try {
+    
+const res = await fetch(api + "/users/register", config)
+.then((res) => res.json())
+.catch((err) => err)
+   
+ // if me  wanna res i get receive user !!
+ if(res){
+ localStorage.setItem("user", JSON.stringify(res))
+ }
+
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+const authService = {
+    registeUser
+}
+
+
+export default authService
