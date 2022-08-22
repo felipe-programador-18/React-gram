a738af56f40998ea57723339d287256c557aa555
Felipe <machadofelipe2016@outlook.com>
2022-08-22 17:55:11 -0300

    c86-doing somethings testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import { useEffect } from 'react';
+
+
+// hook
+import { useAuth } from './hoock/useAuth';
 
 
 //pages
@@ -17,6 +22,17 @@ import Footer from './Component/footer';
 
 
 function App() {
+ 
+ const {auth, loading} = useAuth()
+ console.log('testing auth', auth)
+ 
+ if(loading){
+  <p>Carregando...</p>
+ }
+
+ 
+ 
+ 
   return (
      <div className="App">
      <BrowserRouter>
@@ -25,10 +41,11 @@ function App() {
        
         <div className='container' > 
          <Routes>       
-           <Route path='/' element={<Home/>} />
+           <Route path='/' element={ auth ? <Home/> : <Navigate to='/login' /> } />
         
-           <Route path='/login' element={ <LoginUser/> } />
-           <Route path='/register' element={<RegisterAnother/>}   />
+           <Route path='/login' element={ !auth ?  <LoginUser/> : <Navigate to='/' />} />
+           
+           <Route path='/register' element={!auth ? <RegisterAnother/> : <Navigate to='/'/> }   />
      
          </Routes> 
        </div>
