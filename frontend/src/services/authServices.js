3cfb1b35d58bcbb752b420bf5f0eb850b5d879bb
Felipe <machadofelipe2016@outlook.com>
2022-08-23 11:42:08 -0300

    c92-created all structure to receive dates about my api

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -25,12 +25,31 @@ const logout = () => {
   localStorage.removeItem('user')
 }
 
+// sing in login users!!
+const login = async (data) => {
+  const config = requestConf("POST", data)
+  
+ try {
+  const res = await fetch(api + "/users/login" + config)
+  .then((res) => res.json())
+  .catch((err) => err)
+
+  if(res){
+    localStorage.setItem("user", JSON.stringify(res))
+  }
+  return res;
+  
+ } catch (error) {
+  console.log(error)
+ }
+}
 
 
 
 const authService = {
     registeUser,
-    logout
+    logout,
+    login
 }
 
 export default authService
