53c16b1100b3a3c058e626a27f7debac6ae6c76e
Felipe <machadofelipe2016@outlook.com>
2022-08-27 16:55:48 -0300

    c121-created folder profile user

---
+++ frontend/src/Pages/Profile/profile.user.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./profile.css"
+
+import { upload } from '../../utils/config'
+import {Link, useParams} from 'react-router-dom'
+
+import { BsFillEyeFill, BsPencilFill, BsXLg } from 'react-icons/bs'
+import {useState, useEffect, useRef} from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+
+
+const ProfileUser = () => {
+
+    return( <div>
+
+    </div> )
+}
+export default ProfileUser
