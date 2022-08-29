b38c02d27c4d64f6538e47d176808b730c40aa74
Felipe <machadofelipe2016@outlook.com>
2022-08-29 16:07:58 -0300

    c136- adding and creating function to get details and show user

--- frontend/src/Pages/Profile/profile.user.js
+++ frontend/src/Pages/Profile/profile.user.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import "./profile.css"
 
-import { upload } from '../../utils/config'
+import { uploads } from '../../utils/config'
+
 import {Link, useParams} from 'react-router-dom'
 
 import { BsFillEyeFill, BsPencilFill, BsXLg } from 'react-icons/bs'
@@ -9,11 +10,40 @@ import {useState, useEffect, useRef} from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
 
+import { getUserId } from '../../slices/userSlice'
 
 const ProfileUser = () => {
+    const {id} = useParams()
 
-    return( <div>
+    const dispatch = useDispatch()
+    const {user ,loading}= useSelector((state) => state.user) 
+    const {user: userAuth} = useSelector((state) => state.auth)
+   
+    //photo
 
+
+    //loading user date!
+
+    useEffect(() => {
+    dispatch(getUserId(id))
+    },[dispatch,id])
+
+
+    if(loading){
+        return <p>Carregando profile....</p>
+    }
+
+    return(<div id='profile'>
+     <div className='profile-header'>
+        {user.profileImage && (
+        <img src={`${uploads}/users/${user.profileImage}`}  alt={user.name}  />
+        )}
+        <div className='profile-description'>
+         <h2>{user.name}</h2>
+         <p>{user.bio}</p>
+        </div>
+
+     </div>         
     </div> )
 }
 export default ProfileUser
