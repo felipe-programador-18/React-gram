dbea80743b18dbc546fe8d660a3595da42c96632
Felipe <machadofelipe2016@outlook.com>
2022-08-30 20:16:39 -0300

    c141-get starting create all structure of my applciation

---
+++ frontend/src/slices/photoSlice.js
@@ -0,0 +1,79 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import photoService from "../services/photoService";
+
+
+const initialState ={
+    photos:[],
+    photo:{},
+    error: false,
+    success: false,
+    loading: false,
+    message: null
+}
+
+// creating function to adding photo
+export const publishPhoto = createAsyncThunk("photo/publish",
+ async(photo, thunkAPI) => {
+  
+   const token = thunkAPI.getState().auth.user.token;
+   const data =  await photoService.publishPhoto(photo, token)
+ 
+   if(data.errors){
+    return thunkAPI.rejectWithValue(data.errors[0])
+   }
+   
+   return data;
+ }
+)
+
+
+export const getUserPhoto = createAsyncThunk("photo/userphotos",
+ async(id, thunkAPI) => {  
+  const token = thunkAPI.getState().auth.user.token ;
+  const data = await photoService.getPhotoId(id,token)
+  return data;
+
+ }
+
+ )
+
+
+export const photoSlice =  createSlice({
+    name:"photo" ,
+    initialState,
+    reducers:{
+        resetMessage: (state) =>{
+          state.message = null;
+        },
+    },
+ 
+     extraReducers:(builder) => {
+        builder.addCase(publishPhoto.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        }).addCase(publishPhoto.fulfilled, (state,action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.photo = action.payload;
+            state.photos.unshift(state.photo)
+            state.message = "Photo atualizada com sucesso!"
+        }).addCase(publishPhoto.rejected, (state,action) => {
+            state.loading = false;
+            state.error = action.payload;
+            state.photo ={};
+        }).addCase(getUserPhoto.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        }).addCase(getUserPhoto.fulfilled, (state,action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.photos = action.payload;
+        })
+     }   
+})
+
+export const {resetMessage} = photoSlice.actions
+export default photoSlice.reducer
