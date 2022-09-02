ab974e6dcb2d720fabedde518171642d1e72051a
Felipe <machadofelipe2016@outlook.com>
2022-09-02 17:28:29 -0300

    c166-creating seperate component to together work, but alone

---
+++ frontend/src/Component/photoItem.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./photoItem.css"
+import { Link } from 'react-router-dom'
+
+
+import { uploads } from '../utils/config'
+
+
+const PhotoItem = ({ photo }) => {
+  console.log("photo here now", photo)
+
+    return(<div className='photo-item' >
+        <h1>Foto do usuário</h1>
+
+       {photo.image && (
+           <img src={`${uploads}/photos/${photo.image}`} alt={photo.title}/>
+        )}  
+        
+        <h2>{photo.title}</h2>
+        <p className="photo-author" >
+          <span> 
+           Publicada por: <Link to={`/users/${photo.userId}`} > {photo.userName}  </Link>  
+          </span>   
+        </p> 
+
+    </div>)
+
+}
+
+export default PhotoItem
