de76573d12bed04ebd59bdba32008d070265211b
Felipe <machadofelipe2016@outlook.com>
2022-09-02 17:29:38 -0300

    c168-created like component

---
+++ frontend/src/Component/likeContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./like.container.css"
+
+import { BsHeart, BsHeartFill } from 'react-icons/bs'
+
+
+
+const Like = ({photo, user, handleLike }) =>{
+
+
+return(<div className='like' >
+      {photo.likes && user && (<>
+          {photo.likes.includes(user._id)? (
+            <BsHeartFill/>
+          ) : (
+            <BsHeart onClick={() => handleLike(photo)} />
+          ) }
+
+        <p>  {photo.likes.length}  like(s).  </p>
+      </>)}  
+
+</div>)
+
+}
+
+export default Like
