d28108faab6be2212d01951ff1d44dcbc460a422
Felipe <machadofelipe2016@outlook.com>
2022-09-02 17:36:16 -0300

    c173-created dashboards of photo

--- frontend/src/Pages/Photo/Photo.js
+++ frontend/src/Pages/Photo/Photo.js
@@ -4,18 +4,46 @@ import { useParams, link } from 'react-router-dom'
 
 import { uploads } from '../../utils/config'
 import Message  from '../../Component/message'
+import PhotoItem from '../../Component/photoItem'
 
 import { useSelector, useDispatch } from 'react-redux'
+import {getUserPhotoId,likeHere} from '../../slices/photoSlice'
+import Like from '../../Component/likeContainer'
 
 
 const PhotoUser = () => {
  
-    const {id} = useParams()
+  const {id} = useParams()
+  const dispatch = useDispatch()
+  
+  const {user} = useSelector((state) => state.auth)
+  const {photo, loading, error, message} = useSelector((state) => state.photo)
+
+  //later I need make function to loading photo
+
+
+  //loading date
+  useEffect(() => {
+   dispatch(getUserPhotoId(id))
+},[dispatch,id])
+
+
+// like function and comments
+  const handleLike = () => {
+    dispatch(likeHere(photo._id))
+    
+  }
  
+   
+  if(loading){
+    return <p>Carregando photos.....</p>
+  }
+ 
+ return(<div id="photo" >
 
- return(<div>
-
-     <p>Photo of user here!!!</p>
+     <p>Mural de Fotos.</p>
+     <PhotoItem photo={photo} />
+     <Like photo={photo} user={user} handleLike={handleLike} />
 
     </div>)
 
