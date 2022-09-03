028d6101edb97a251be48cb5c19021ccd5df05e1
Felipe <machadofelipe2016@outlook.com>
2022-09-03 16:33:19 -0300

    c192-doing someone test inside component

--- frontend/src/Pages/Photo/Photo.js
+++ frontend/src/Pages/Photo/Photo.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import "./photo.css"
-import { useParams, link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 
 import { uploads } from '../../utils/config'
@@ -8,7 +8,7 @@ import Message  from '../../Component/message'
 import PhotoItem from '../../Component/photoItem'
 
 import { useSelector, useDispatch } from 'react-redux'
-import {getUserPhotoId,likeHere} from '../../slices/photoSlice'
+import {getUserPhotoId,likeHere, comment } from '../../slices/photoSlice'
 import Like from '../../Component/likeContainer'
 import { useResetComponent } from '../../hoock/useResetcomponent'
 
@@ -22,22 +22,37 @@ const PhotoUser = () => {
 
   const {user} = useSelector((state) => state.auth)
   const {photo, loading, error, message} = useSelector((state) => state.photo)
+  const {error: AnotherError} = useSelector((state) => state.auth)
 
-  //later I need make function to loading photo
-
+  //comments here
+  const [commentText, setCommentsText] = useState("")
 
   //loading date
   useEffect(() => {
    dispatch(getUserPhotoId(id))
 },[dispatch,id])
 
+  
+ 
 
-// like function and comments
+// like function and reset dates!!
   const handleLike = () => {
     dispatch(likeHere(photo._id))
    resetMess()
   }
  
+  //insert comments
+  const handComments =  (e) => {
+    e.preventDefault()
+    const photoData = {
+      comment: commentText,
+      id: photo._id,
+    }
+
+    dispatch(comment(photoData))
+    setCommentsText("")
+    resetMess()
+  }
    
   if(loading){
     return <p>Carregando photos.....</p>
@@ -53,6 +68,48 @@ const PhotoUser = () => {
         {error && <Message msg={error} type='error' />}
         {message && <Message msg={message} type='success'  /> }
      </div>
+     
+     <div className='comments' >
+     
+     {photo.comments && (<>
+     
+      <h3>Comentários
+        ({photo.comments.length}):
+      </h3>
+      <form onSubmit={handComments}>
+        <input 
+        type="text" 
+        placeholder='Escreva seu comentário.' 
+        onChange={(e) => setCommentsText(e.target.value)}
+        value={commentText || ""}  />
+       
+       <input type="submit" value='Enviar Comentário.' />
+       {error && <Message msg={error} type='error' />}
+      </form>
+       
+       {photo.comments.length === 0 && <p>Não há comentário Aqui.</p>}
+       {photo.comments.map((comment) => (
+        <div className='comment' key={comment.comment}>
+          <div className='author'>
+            {comment.userImage && (
+              <img src={`${uploads}/users/${comment.userImage}`} 
+              alt={comment.userName} />
+            )}
+          </div>  
+          <Link to={`/users/${comment.userId}`} >
+             <p>{comment.userName}</p>  
+          </Link> 
+             <p>{comment.comment}</p>
+            
+        </div>
+       )) }
+     
+     </>)}
+     
+     
+     </div>
+
+
 
     </div>)
 
