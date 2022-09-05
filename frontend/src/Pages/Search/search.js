fc25c419609d12755c7f4f3a083860cd7a752c4b
Felipe <machadofelipe2016@outlook.com>
2022-09-05 13:03:33 -0300

    c214-set up last touch inside this code

--- frontend/src/Pages/Search/search.js
+++ frontend/src/Pages/Search/search.js
@@ -5,17 +5,59 @@ import { useQuerySearch } from "../../hoock/useQuery"
 import Like from '../../Component/likeContainer'
 import PhotoItem from '../../Component/photoItem'
 
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+
+import { likeHere, SearchPhoto } from "../../slices/photoSlice"
 
 import {Link} from 'react-router-dom'
 
 
 const SearchDates = () => {
-     const {search} = useQuerySearch()  
+    const query = useQuerySearch()  
+    const search = query.get("q")
+    
+    const dispatch = useDispatch()
+    const resetMessage = useResetComponent(dispatch)
 
-    return ( <div>
-         <h2>Search here</h2>
+    const {user} = useSelector((state) => state.auth)
+    const {photos, loading} = useSelector((state) => state.photo)
+     
+    useEffect(() => {
+    dispatch(SearchPhoto(search))
+    },[dispatch, search]) 
+    
+     //like photo
+     const handleLike = (photo = null) => {
+        dispatch(likeHere(photo._id))
+        resetMessage()
+     }
+   
+       if(loading){
+        return <p>Carregando..</p>
+       }
+
+    return ( <div id="search" >
+        
+        <h2>Você está buscando por:{search}</h2> 
+          {photos && photos.map((photo) => (
+            <div key={photo._id}>
+              <PhotoItem photo={photo} />
+              <Like photo={photo} user={user} handleLike={handleLike} />
+              <Link className='btn' to={`/photos/${photo._id}`} >Ver Mais.</Link>
+
+            </div>
+            ))
+          }
+
+          {photos && photos.length === 0 && (
+           <> 
+             <h2 className='no-photos' >Não foram encontradas nenhuma foto aqui.</h2>
+            
+            </>
+          )}
+         
+
 
     </div> )
 }
